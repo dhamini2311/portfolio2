121bdd25e2ae2fea9962571f059147aa02cf8032
dham2311 <dhamini2311@gmail.com>
2023-10-05 22:19:11 +0530

    latest commit

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,9 +2,47 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  templateUrl: './projects.component.html',
+  templateUrl:'./projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  totalProjects = 3;  // Total projects in your dataset
+  projectsPerPage = 1;  // Projects to display per page
+  currentPage = 1;
+  totalPages: number=0;
+  pagedProjects: string[]=[];  // Replace with your actual data type
 
+  constructor() {
+    this.calculateTotalPages();
+    this.loadPage(this.currentPage);
+  }
+
+  calculateTotalPages(): void {
+    this.totalPages = Math.ceil(this.totalProjects / this.projectsPerPage);
+  }
+
+  loadPage(page: number): void {
+    // Replace with your data loading logic based on the page number
+    // Example: Load data from a service using an HTTP request
+    this.pagedProjects = [];  // Replace with your actual data
+    for (let i = (page - 1) * this.projectsPerPage; i < page * this.projectsPerPage; i++) {
+      if (i < this.totalProjects) {
+        this.pagedProjects.push(`Project ${i + 1}`);
+      }
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.loadPage(this.currentPage);
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadPage(this.currentPage);
+    }
+  }
 }
